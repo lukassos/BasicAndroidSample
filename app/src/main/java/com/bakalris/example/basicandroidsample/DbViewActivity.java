b1a3901cdd929e703bacba36c93c2fe7ed1da5b8
lukassos <lukassos@gmail.com>
2016-04-06 17:07:39 +0200

    menu

--- app/src/main/java/com/bakalris/example/basicandroidsample/DbViewActivity.java
+++ app/src/main/java/com/bakalris/example/basicandroidsample/DbViewActivity.java
@@ -3,6 +3,8 @@ package com.bakalris.example.basicandroidsample;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -51,5 +53,28 @@ public class DbViewActivity extends AppCompatActivity {
     private void deleteAll() {
         Kofola.deleteAll(Kofola.class);
         Toast.makeText(DbViewActivity.this, "Deletet all Kofolas", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.database_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            deleteAll();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
